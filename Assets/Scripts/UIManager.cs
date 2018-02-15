8e423e3cd09c49138ecb9c7dc211ade075b180b5
CARIVASH <rivsasterpiece@gmail.com>
2018-02-14 21:56:47 -0600

    Add Empty Project

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour {
+
+	public GameObject txt;
+	public bool clic;
+	// Use this for initialization
+	void Start () {
+		txt.SetActive (false);
+		Screen.SetResolution (1080,1920,true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (clic) {
+			txt.SetActive (true);
+		} else {
+			txt.SetActive (false);
+		}
+	}
+
+	public void onClickButton(){
+		clic = !clic;
+	}
+}
